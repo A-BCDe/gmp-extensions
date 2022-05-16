63bef317b314d8c8dedebb2ddf1a32a7016dc37b
A-BCDe <a.bcde.creator@gmail.com>
2022-05-16 14:26:30 +0900

    Added one more example, which the given example of factorizing modulo p doesn't work well.

--- app/polynomial.cc
+++ app/polynomial.cc
@@ -25,11 +25,15 @@ int main() {
 	//project::integer_polynomial poly(std::vector<std::string>{ "6", "0", "3", "2", "0", "1" });
 	// (x^2 + 2)(x^2 - 2)
 	//project::integer_polynomial poly(std::vector<std::string>{ "-4", "0", "0", "0", "1" });
+	// (3x + 1)(3x + 2)(3x + 3)(3x + 4)
+	project::integer_polynomial poly(std::vector<std::string>{ "24", "150", "315", "270", "81" });
 	// (2x^2 + 3x + 4)(7x^2 - x + 53)
-	project::integer_polynomial poly(std::vector<std::string>{ "212", "155", "131", "19", "14" });
+	//project::integer_polynomial poly(std::vector<std::string>{ "212", "155", "131", "19", "14" });
 	// (x^18 - 1)
 	//project::integer_polynomial poly(std::vector<std::string>{ "-1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1" });
 
+/* A code for factorizing modulo p
+ * Should not work for the test case (3x + 1)(3x + 2)(3x + 3)(3x + 4), as the leading coefficient is zero.
 	mpz_class const p = 3;
 	std::cout << poly << '\n';
 	auto factors = poly.factorize(p);
@@ -37,9 +41,9 @@ int main() {
 	for(auto const &v : factors) {
 		std::cout << v << '\n';
 	}
-
+*/
 	auto start = std::chrono::system_clock::now();
-	factors = poly.factorize();
+	auto factors = poly.factorize();
 	std::chrono::duration<double> sec = std::chrono::system_clock::now() - start;
 	std::cout << "Irreducible factors of " << poly << " are:\n";
 	for(auto const &v : factors) {
